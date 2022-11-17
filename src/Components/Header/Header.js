0fa9e3d78c1f6a25cde264fb319485df44fff3ce
Aditya Rawal <ar.adityarawal@gmail.com>
2022-11-18 02:17:00 +0530

    Added required Png files and started Header Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../Logo/Logo.js'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+        <nav className='Header'>
+        
+        <Logo />
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Logo() {
+  return (
+    <>
+    <a href='http://localhost:3000/'> 
+        <div>
+            <div>
+                <img src='svgexport-1.png' alt='Logo'></img>
+                <img src='svgexport-1.png' alt='Logo-Side'></img>
+            </div>
+        </div>
+    </a>
+    </>
+  )
+}
+
+export default Logo
